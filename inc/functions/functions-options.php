65cbe14b898770ae04c7543dcadc278d6fd91d2d
Dennis <dennisthompson@atomicpages.net>
2015-01-12 20:44:38 -0800

    Finally connected redux to the theme

---
+++ inc/functions/functions-options.php
@@ -0,0 +1,18 @@
+<?php
+
+// code for image loaders (e.g. colorbox, fancybox, any other developer additions)
+// code for sliders (e.g. flexslider, responsiveslides, any other developer additions)
+// any other code that needs to be loaded from the administrative panel goes in here...
+
+global $skeleton_wp; // connects redux goodness
+
+/**
+ * Determines if an options exists or is set to something other than a blank value
+ * @param string $option
+ * @return bool
+ */
+function skeleton_wp_option_isset($option) {
+	global $skeleton_wp;
+	if(array_key_exists($option, $skeleton_wp) && isset($skeleton_wp[$option])) return true;
+	return false;
+}
